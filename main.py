f00ca750c0e6b7af60b3c5bc295a46763c6f838a
Ben Arnao <arnaoben@gmail.com>
2020-02-06 11:57:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -30,18 +30,32 @@ def timestamp_in_candle_bounds(trade_timestamp, candle_open_timestamp, candle_cl
         return True
     return False
 
+class TradesContainer:
+    def __init__(self, first_trade_ts, ms_in_candle):
+        self.candle_open -= first_trade_ts % ms_in_candle
+        self.candle_close = candle_open + ms_in_candle
+        self.ms_in_candle = ms_in_candle
+        self.candles = []
+    
+    def move_to_next_candle(self)
+        self.candle_open_timestamp += self.ms_in_candle
+        self.candle_close_timestamp += self.ms_in_candle
+     
+    def add_candle(self, candle)
+        self.candles.append(candle)
+     
+    def get_candles(self)
+        return self.candles
+
 #assumes all time given in ms timestamp format
 def make_candles(trades, ms_in_candle):
-    candle_open = trades[0].time
-    candle_open -= candle_open % ms_in_candle
-    candle_close = candle_open + ms_in_candle
-    candles, trades_in_candle = [], []
-    
     trades.sort(key=lambda x: x.time)
+        
+    tc = TradesContainer(trades[0].time, ms_in_candle)
+    trades_in_candle = []
     
     # this method automatically ignores earliest candle as incomplete and does not make a candle until it's seen a trade outside of the candle's range.
     # Gaurenteeing intregrity of all candles.
-
 
     for idx, trade in enumerate(trades):
         if timestamp_in_candle_bounds(trade.time, candle_open, candle_close):
@@ -49,10 +63,10 @@ def make_candles(trades, ms_in_candle):
             trades_in_candle.append(Trade(trade.time, trade.qty, trade.price, trade.id))
         else:
             # create candle and add
-            candles.append(Candle(candle_close, trades_in_candle))
+            tc.add_candle(Candle(candle_close, trades_in_candle))
 
             # move to next trades_in_candle
-            candle_open, candle_close = next_candle(candle_open, candle_close, ms_in_candle)
+            tc.move_to_next_candle()
 
             # if trade doesn't fit in next consecutive candle (too far in futue), add a dummy candle and then open a new candle
             while not timestamp_in_candle_bounds(trade.time, candle_open, candle_close):
@@ -60,11 +74,11 @@ def make_candles(trades, ms_in_candle):
                 dummy_candle = copy.deepcopy(candles[-1])
                 dummy_candle.close_time = candle_close
                 dummy_candle.volume = 0
-                candles.append(dummy_candle)
+                tc.add_candle(dummy_candle)
 
                 # move to next trades_in_candle
-                candle_open, candle_close = next_candle(candle_open, candle_close, ms_in_candle)
+                tc.move_to_next_candle()
 
             # create new trades_in_candle with curr trade
             trades_in_candle = [Trade(trade.time, trade.qty, trade.price, trade.id)]
-    return candles
+    return tc.get_candles()
